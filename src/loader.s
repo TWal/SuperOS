1aeb9264124765fa07948428c041aa343d8e3245
Thibaut Perami <perami@clipper.ens.fr>
2017-01-27 21:08:31 +0100

    Hello world !

--- src/loader.s
+++ src/loader.s
@@ -2,13 +2,21 @@
 
 .equ MAGIC_NUMBER, 0x1BADB002
 .equ FLAGS, 0x0
-.equ CHECKSUM, -MAGIC_NUMBER
+.equ CHECKSUM, -MAGIC_NUMBER - FLAGS
 # MAGICNUMBER + FLAGS + CHECKSUM = 0
+
+.equ STACKSIZE, 4096
+
+.bss
+kernel_stack:
+  .space STACKSIZE
+
 
 .text
 .int MAGIC_NUMBER, FLAGS, CHECKSUM
 
 loader:
-    mov $0xCAFEBABE, %eax
+  mov $(STACKSIZE + kernel_stack),%esp
+  call kmain
 loop:
-    jmp loop
+  jmp loop
